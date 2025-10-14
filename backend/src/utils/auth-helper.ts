96858b8762ae47fd1095be4ff491ed91367ef18d
Dexter2-z <chinazachinyere7@gmail.com>
2025-10-14 02:24:36 +0100

    another bug fixed

--- backend/src/utils/auth-helper.ts
+++ backend/src/utils/auth-helper.ts
@@ -3,9 +3,9 @@ import { ValidationError } from "../packages/error-handler";
 const emailRegex = /^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_'+\-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/i;
 
 export const validateRegistrationData = (data: any, userType: "admin") => {
-    const {name, email, password, phone_number} = data;
+    const {name, email, phone_number} = data;
 
-    if(!name || !email || !password || (userType === "admin" && (!phone_number))){
+    if(!name || !email || (userType === "admin" && (!phone_number))){
         throw new ValidationError(`Missing Required Fields`)
     }
 
