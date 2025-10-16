6573f7215f7d03d5eb0af2913b957c892972330c
chimdykekamsi <ckamsi04@gmail.com>
2025-10-16 02:16:56 +0100

    chore(api): prepare for merge

--- backend/src/modules/admins/admin.controller.ts
+++ backend/src/modules/admins/admin.controller.ts
@@ -2,43 +2,43 @@ import jwt from "jsonwebtoken";
 import prisma from "../../packages/libs/prisma";
 import { NextFunction, Request, Response } from "express";
 import { validateRegistrationData } from "../../utils/auth-helper";
-import{ sendCustomEmail, getPatientWelcomeEmail } from '../../utils/send-email';
+import { sendCustomEmail, getPatientWelcomeEmail } from '../../utils/send-email';
 
 function generateRandomPassword(length = 8) {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let pwd = '';
-  for (let i = 0; i < length; i++) {
-    pwd += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return pwd;
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let pwd = '';
+    for (let i = 0; i < length; i++) {
+        pwd += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return pwd;
 }
 
 //Create admin
 export const createAdmin = async (req: Request, res: Response, next: NextFunction) => {
     try {
         validateRegistrationData(req.body, "admin");
-        const {name, email, phone_number} = req.body;
+        const { name, email, phoneNumber } = req.body;
         const password = generateRandomPassword();
 
         const existingAdmin = await prisma.admin.findUnique({
-            where: {email}
+            where: { email }
         });
 
-        if(existingAdmin){
-            return res.status(400).json({message: "Admin with this email already exists"});
+        if (existingAdmin) {
+            return res.status(400).json({ message: "Admin with this email already exists" });
         }
 
-        if(!existingAdmin){
+        if (!existingAdmin) {
             const newAdmin = await prisma.admin.create({
                 data: {
                     name,
                     email,
                     password,
-                    phone_number
+                    phoneNumber
                 }
             });
 
-            if(newAdmin){
+            if (newAdmin) {
                 // Send email with credentials
                 const emailContent = await getPatientWelcomeEmail({ full_name: name, email, password });
                 await sendCustomEmail({ to: email, ...emailContent });
@@ -46,7 +46,7 @@ export const createAdmin = async (req: Request, res: Response, next: NextFunctio
 
             return res.status(201).json({
                 success: true,
-                message: "Admin created successfully", 
+                message: "Admin created successfully",
                 admin: newAdmin
             });
         }
