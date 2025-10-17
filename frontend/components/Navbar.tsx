8aca76f875ddcb0c5f47effd78003dcd97bc3222
Ogbuagu Great <105602085+adgxdev@users.noreply.github.com>
2025-10-17 18:14:08 +0100

    Short (#9)
    * feature: implemented the landing/waitlist page
    
    * refactor: fixed font displacement issue on mobile

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
                     <h1 className="text-lg bold-text">Booka</h1>
                 </Link>
                 <div className="">
-                    <Link href="/about" className="bg-black text-white rounded-lg py-2.5 px-3 text-sm flex gap-2 items-center bold-text hover:scale-102 duration-500">
+                    <Link href="/about" className="bg-black text-white rounded-lg py-2.5 px-3 text-sm flex gap-2 items-center justify-center font-bold hover:scale-102 duration-500">
                         Get the App<span className="uppercase text-xs bg-white rounded text-black px-2 py-0.5">Soon</span></Link>
                 </div>
             </div>
