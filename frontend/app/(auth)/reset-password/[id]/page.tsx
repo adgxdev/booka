72020f9558646ea781e128cd34c661cea278190b
adgdevv <opponamer@gmail.com>
2025-11-05 16:11:03 +0100

    feature: worked on the authentication flows

--- frontend/app/(auth)/reset-password/[id]/page.tsx
+++ frontend/app/(auth)/reset-password/[id]/page.tsx
@@ -2,11 +2,11 @@
 import NewPassForm from "@/components/auth/NewPassForm";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
-import { use, useEffect, useState } from "react";
+import { use, useEffect } from "react";
 
 export default function ResetPasswordPage({ params, }: { params: Promise<{ id: string }> }) {
     const { id } = use(params)
-    const [isLoading, setIsLoading] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
     // const [isAllowed, setIsAllowed] = useState(false);
 
     // check the dynamic id param for changing password
@@ -15,13 +15,13 @@ export default function ResetPasswordPage({ params, }: { params: Promise<{ id: s
         console.log("Resetting password for user ID:", id);
     }, [id]);
 
-    if(isLoading){
-        return (
-            <div>
-                <h1>Loading...</h1>
-            </div>
-        )
-    }
+    // if(isLoading){
+    //     return (
+    //         <div>
+    //             <h1>Loading...</h1>
+    //         </div>
+    //     )
+    // }
 
     return (
         <main className="w-full h-screen text-white relative">
