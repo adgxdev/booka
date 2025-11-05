1f632b332686b63944be89fd13b772992f63b98c
adgdevv <opponamer@gmail.com>
2025-11-05 15:34:41 +0100

    feature: worked on the authentication flows

---
+++ frontend/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function ForgotPasswordPage() {
+
+    // const sendPasswordResetLink = async (email: string) => {
+    //     // Implement the logic to send a password reset link to the provided email
+    //     console.log(`Sending password reset link to ${email}`);
+    // }
+
+    return (
+        <main className="w-full h-screen text-white relative">
+            <Link href={'#'} className="fixed left-5 top-6">
+                <ChevronLeft className="h-8 w-8 text-[#00C6FF]" />
+            </Link>
+            <div className="z-30 h-full w-11/12 mx-auto flex flex-col items-center pt-30">
+                <div className="w-full flex flex-col justify-center text-center">
+                    <Link href={'/'} className="">
+                        <h1 className="text-[#00C6FF] text-3xl font-bold bold-text mb-6">Booka</h1>
+                    </Link>
+                    <h1 className="text-2xl font-semibold text-[#00C6FF]">Forgot your password?</h1>
+                    <p className="text-sm my-4">Enter your email address  and we will <br className="md:hidden"/>send you a link to reset your password </p> 
+                </div>
+                <ForgotPasswordForm />
+            </div>
+        </main>
+    );
+}
--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,10 +1,16 @@
+import SignupForm from "@/components/auth/SignupForm";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function SignupPage() {
     return (
-        <main>
-            <section>
-                Signup
-                <label>Name</label>
-            </section>
+        <main className="h-screen text-white relative">
+            <Link href={'#'} className="fixed left-5 top-6">
+                <ChevronLeft className="h-8 w-8 text-[#00C6FF]" />
+            </Link>
+            <div className="z-30 h-full w-11/12 mx-auto flex flex-col items-center pt-30">
+                <SignupForm />
+            </div>
         </main>
     );
 }
