11e638eb312898ba76ddff50ff9ed5317e000287
chimdykekamsi <ckamsi04@gmail.com>
2025-11-06 12:46:13 +0100

    add waitlist existing data to response

--- backend/src/modules/waitlists/waitlist.controller.ts
+++ backend/src/modules/waitlists/waitlist.controller.ts
@@ -5,7 +5,7 @@ import prisma from "../../configs/prisma";
 import { APIError } from "../../utils/APIError";
 import { APIResponse } from "../../utils/APIResponse";
 import { logger } from "../../utils/logger";
-import { sendCustomEmail, waitlistEmail } from "../../utils/send-email";
+// import { sendCustomEmail, waitlistEmail } from "../../utils/send-email";
 
 export const createWaitlist = async (req: Request, res: Response) => {
     const body = CreateWaitlistDto.parse(req.body);
@@ -23,7 +23,7 @@ export const createWaitlist = async (req: Request, res: Response) => {
             type: "create",
             requestId: req.id
         });
-        throw APIError.Conflict("Email is already registered in the waitlist.");
+        throw APIError.Conflict("Email is already registered in the waitlist.", { entry: existing });
     };
 
     // 2️⃣ Generate unique referral code
