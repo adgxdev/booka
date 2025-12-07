bdefbebf146d63de1c7940064626bdfed686bfc5
chimdykekamsi <ckamsi04@gmail.com>
2025-12-07 14:42:25 +0100

    Refactor email templates and update ImageKit URL endpoint

--- backend/src/utils/imagekit/index.ts
+++ backend/src/utils/imagekit/index.ts
@@ -2,7 +2,7 @@ import ImageKit from "imagekit";
 
 
 export const imagekit = new ImageKit({
-    publicKey : process.env.IMAGEKIT_PUBLIC_KEY!,
-    privateKey : process.env.IMAGEKIT_SECRET_KEY!,
-    urlEndpoint : "https://ik.imagekit.io/dexter"
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
+    privateKey: process.env.IMAGEKIT_SECRET_KEY!,
+    urlEndpoint: "https://ik.imagekit.io/bookacampus"
 });
